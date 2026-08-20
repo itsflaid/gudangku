31ff19639d890120a36891b3351d566e5815a1bd
Mufacoderz <mf9555759@gmail.com>
2026-08-20 20:21:48 +0800

    admin lte

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,42 @@
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item dropdown user-menu">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                        <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="user-image img-circle"
+                            alt="User Image">
+                        <span class="d-none d-md-inline">Prabowo Subianto</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <!-- User image -->
+                        <li class="user-header ">
+                            <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle"
+                                alt="User Image">
+
+                            <p>
+                                Prabowo Subianto - Web Developer
+                                <smal class="badge badge-success">Admin</smal>
+                            </p>
+                        </li>
+                        <!-- Menu Body -->
+
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <a href="#" class="btn btn-sm btn-danger float-right">
+                                <i class="fas fa-sign-out-all"></i>
+                                Log out</a>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,75 @@
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="#" class="brand-link">
+                <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle">
+                <span class="brand-text font-weight-light">Gudangku</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- SidebarSearch Form -->
+
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-home"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+
+                        <li class="nav-header">MENU SUPERADMIN</li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link @yield('menuSuperadminUser')">
+                                <i class="nav-icon fas fa-user"></i>
+                                <p>
+                                User
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-list"></i>
+                                <p>
+                                Kategori
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-warehouse"></i>
+                                <p>
+                                Barang
+                                </p>
+                            </a>
+                        </li>
+
+
+
+
+                        <li class="nav-header">MENU ADMIN</li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-warehouse"></i>
+                                <p>
+                                Barang
+                                </p>
+                            </a>
+                        </li>
+
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
